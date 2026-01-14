65f1edc8dc90a2d97446003acc74f9b8bfee304b
Aaron B. <aaron.bumberger@gmail.com>
2026-01-14 20:42:10 +0100

    updated results

--- foo.R
+++ foo.R
@@ -1,16 +1,17 @@
-# Radius
-r <- 2
 # Function to compute the volume of a sphere with radius r
 volume <- function(r, i = 1) {
   return(4/3*pi*r^(3*i))
 }
 # Function to compute the volumes of the spheres with radius r, r^2 and r^3
 volume_vector <- function(r) {
-  r <- 22
   for (i in 1:3){
     result <- volume(r, i)
     print(result)
   }
 }
+
+# Radius
+#r <- 2
+
 # Run volume_vector(r) and print the volumes of the spheres with radius r, r^2 and r^3
-volume_vector(r)
+volume_vector(2)
